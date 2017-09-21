bcca262437fae788893d596ae6cf819668bed4c4
18513763652 <2738328919@qq.com>
2017-09-21 21:32:08 +0800

    1
    1

--- js/app.js
+++ js/app.js
@@ -38,7 +38,6 @@ var count = 0;
  Player.prototype.update = function (dt) {
    if(this.y === -11){
        count++;
-       //经过测试，经过3ms后，就可以实现到达河岸，并网页输出胜利指示，确定后，即可回到原位。
        if(count%3 === 2){
             alert("good job！！ 点击确定后进入下一盘游戏");
             this.x = 200;
@@ -57,7 +56,6 @@ var count = 0;
 }
 Player.prototype.checkCollisions = function(){
     for(var i=0;i<allEnemies.length;i++){
-        //首先判断player和enemy是否在同一行（此处因为我所设置的player和enemy是每一行所在位置的y坐标是相同的）
         if(this.y === allEnemies[i].y){
             if((Math.abs(this.x - allEnemies[i].x))<40){
                 this.x =200;
@@ -72,10 +70,9 @@ Player.prototype.checkCollisions = function(){
  };
 
  var allEnemies = [];
-//定义6个Enemy，每行两个，每一行的初始位置是固定的
 for(var i=0;i<6;i++){
   var bugs = new Enemy(-30,83*(i%3)+72);
-  //将所有bugs都添加到allEnemies数组中
+
   allEnemies.push(bugs);
 };
     var player = new Player(101, 83 * 3 + 55)
