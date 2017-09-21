a26f666524e77ea9218cf2b31cb25809ec9f087e
18513763652 <2738328919@qq.com>
2017-09-21 21:24:58 +0800

    game fix
    game fix

--- js/app.js
+++ js/app.js
@@ -2,31 +2,28 @@
   var Enemy = function(x, y,speed) {
      this.x = x;
      this.y = y;
-this.speed = speed;
+this.speed =  Math.random()*250+150;
      this.sprite = 'images/enemy-bug.png';
   };
 
  // Update the enemy's position, required method for game
   // Parameter: dt, a time delta between ticks
-   Enemy.prototype.update = function(dt) {
+   Enemy.prototype.update = function(dt,x) {
 
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
      // all computers.
      this.x += dt * this.speed
+     if(this.x >= 505){
+        this.x = -30;
+    }
 };
 
  // Draw the enemy on the screen, required method for game
  Enemy.prototype.render = function() {
      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
  };
-Enemy.prototype.checkCollision = function (player) {
-  if (this.y === player.y) {
-       console.log(`collision happened!! enemy.x: ${this.x}, player.x: ${player.x}`)
-   } else {
-       console.log(`player's safe!! enemy.x: ${this.x}, player.x: ${player.x}`)
-    }
-}
+
  // Now write your own player class
  // This class requires an update(), render() and
 // a handleInput() method.
@@ -37,8 +34,17 @@ var Player = function (x, y) {
    this.y = y;
     this.sprite = 'images/char-boy.png'
  }
-
+var count = 0;
  Player.prototype.update = function (dt) {
+   if(this.y === -11){
+       count++;
+       //经过测试，经过3ms后，就可以实现到达河岸，并网页输出胜利指示，确定后，即可回到原位。
+       if(count%3 === 2){
+            alert("good job！！ 点击确定后进入下一盘游戏");
+            this.x = 200;
+            this.y = 404;
+       }
+    }
 
  }
  Player.prototype.handleInput = function (movement) {
@@ -49,11 +55,29 @@ var Player = function (x, y) {
        case 'down': this.y += 83; break;
     }
 }
+Player.prototype.checkCollisions = function(){
+    for(var i=0;i<allEnemies.length;i++){
+        //首先判断player和enemy是否在同一行（此处因为我所设置的player和enemy是每一行所在位置的y坐标是相同的）
+        if(this.y === allEnemies[i].y){
+            if((Math.abs(this.x - allEnemies[i].x))<40){
+                this.x =200;
+                this.y =404;
+            }
+       }
+    }
+};
+
  Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
  };
- 
-   var allEnemies = [new Enemy(0, 83 * 2 + 55, 200)]
+
+ var allEnemies = [];
+//定义6个Enemy，每行两个，每一行的初始位置是固定的
+for(var i=0;i<6;i++){
+  var bugs = new Enemy(-30,83*(i%3)+72);
+  //将所有bugs都添加到allEnemies数组中
+  allEnemies.push(bugs);
+};
     var player = new Player(101, 83 * 3 + 55)
  // Now instantiate your objects.
  // Place all enemy objects in an array called allEnemies
